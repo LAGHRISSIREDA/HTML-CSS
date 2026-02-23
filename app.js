3e48d25c0262a709a6556580fc42030929ae92ee
Xoder <laghrissi.reda01@gmail.com>
2026-02-23 04:49:59 +0100

    JSX Examples

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-const element  = React.createElement('div',null,React.createElement('h1',null,'Salam'));
-ReactDOM.render(element,document.getElementById('app'))
+const fullName = {
+    firstName : "Reda",
+    lastName : "Laghrissi"
+}
+function getName(name){
+    return name.firstName +' °° '+name.lastName
+}
+const testValue = true;
+const inslineStyle = {
+    color : 'red',
+    fontSize : '9px'
+}
+const element = (
+    <div>
+        <h1>Hello</h1>
+        <h2>World !!</h2>
+        <p style={inslineStyle}>Test Paragraph</p>
+    </div>
+)
+ReactDOM.createRoot(document.getElementById('app')).render(element)
+console.log('test')
