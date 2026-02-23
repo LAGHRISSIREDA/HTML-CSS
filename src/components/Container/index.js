9bbe1ea190f07353aecc23e720ad3a93e2d84104
Xoder <laghrissi.reda01@gmail.com>
2026-02-23 23:44:12 +0100

    Update style and index file

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -1,9 +1,14 @@
 //Container Component
 import './style.css'
+import {styles} from '../style'
 export default function ContainerComponent() {
   return (
-    <div className="container">
-      <p>
+    <div className={`container`}>
+      <p style={200>23?{...styles.paragraphStyle}:{}}>
+        Et proident cillum voluptate duis ea quis consectetur Lorem esse
+        exercitation.
+      </p>
+      <p style={styles.divStyle}>
         Et proident cillum voluptate duis ea quis consectetur Lorem esse
         exercitation.
       </p>
---
+++ src/components/style.js
@@ -0,0 +1,15 @@
+
+
+export const styles = {
+    divStyle : {
+    color : "white",
+    background : "#ccc",
+    padding : "50px"
+    },
+    paragraphStyle:{
+    color : "red",
+    background : "#ccc",
+    border : "1px solid #6e6e6e",
+    padding : "50px"
+    }
+}
